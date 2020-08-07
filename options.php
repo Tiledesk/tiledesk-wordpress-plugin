26093dfb2234b0fb98f0cf300f523325957efc7e
Andrea Leo <andrealeo83@users.noreply.github.com>
2020-08-07 13:40:49 +0200

    Add files via upload

---
+++ options.php
@@ -0,0 +1,46 @@
+<div id="tiledesk-wrapper">
+
+    <div id="tiledesk-chat-wp-plugin">
+        
+        <div id="welcome-text">
+            <h1>Hi there! Great to see you’ve installed Tiledesk Chat.</h1>
+            <p>Select one of the options below and your integration will be complete. If you’ve already got a Tiledesk account, the current project will be connected to that account. However, if you don’t have an account with us (or you’d like to create a new one) - just select the option on the right.</p>
+        </div>
+        <div id="after-install-text">
+            <h1>Your site is already integrated with Tiledesk Chat</h1>
+            <p>All you need to do now is select the “Tiledesk Chat” tab on the left - that will take you to your Tiledesk panel. You can also open the panel by using the link below.</p>
+            <p><a href="#" id="open-panel-link" class="button button-primary tiledesk-button" target="_blank" style="margin-left: auto; margin-right: auto; display: block;">Go to Panel</a></p>
+        </div>
+        
+        
+        <div class="error"></div>
+        
+        <div id="input-blocks">
+            <div id="tiledesk-chat-login">
+                <h2>Yes, I have an account with Tiledesk</h2>
+                <div class="form-control">
+                    <input type="text" value="" id="tiledesk-login-input" class="text-input" placeholder="Enter your e-mail...">
+                </div>
+                <div class="form-control">
+                    <input type="password" value="" id="tiledesk-password-input" class="text-input" placeholder="Type password...">
+                </div>
+                <a href="#" id="tiledesk-login-button" class="button tiledesk-button" target="_blank">Login</a>
+            </div>
+            <div id="tiledesk-chat-panel">
+                <h2>No, I do not have an account with Tiledesk</h2>
+                <div id="tiledesk-apply-account">
+                    That's alright, you can start using Tiledesk without having the login.
+                </div>
+                <a href="https://support.tiledesk.com/dashboard/#/signup" id="redirect-to-panel" class="button button-primary tiledesk-button" target="_blank">Create a new account</a>
+            </div>
+        </div>
+        
+        <div id="select-project">
+            <h2>Select project:</h2>
+            <div class="form-control">
+                <select name="select-tiledesk-project" id="select-tiledesk-project"></select>
+            </div>
+            <a href="#" id="get-tiledesk-project" style="margin-left:auto;margin-right:auto;display:block;" class="button button-primary tiledesk-button" target="_blank">Select</a>
+        </div>
+    </div>
+</div>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Tiledesk Live Chat ===
+Contributors: @tiledesk
+Tags: free live chat, plugin, facebook, admin, live chat, chat, livechat, post, tiledesk, widget, post, posts, twitter, zendesk, mailchimp
+Requires at least: 3.0
+Tested up to: 5.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Tiledesk Live Chat – Free live chat for your website with integrated Bot. It integrates into your site in just a few clicks.
+
+== Description ==
+
+Tiledesk is a free live chat service with intelligent Bots ready to use in minutes.
+
+
+== Installation ==
+1. Go to WordPress Control Panel
+1. Click "Plugins", then "Add New"
+1. Type in "Tiledesk Live Chat" and click "Search Plugins"
+1. Download and install the Plugin
+1. Click the "Activate Plugin" link
+1. Move to the "Live Chat" section in the menu
+1. Done! Now you can start talk with your users
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+
+
+= 1.0 =
+* Initial release.
+
+
+== Upgrade Notice ==
+= 1.0 =
+* Initial release.
