2b1a488cc7860bdc6517fd94bb207a925ad7292f
Andrea Leo <andrealeo83@users.noreply.github.com>
2020-08-07 13:40:25 +0200

    Add files via upload

---
+++ classes/TiledeskOneApi.php
@@ -0,0 +1,35 @@
+<?php
+
+class TiledeskOneApi {
+
+    private $publicKey;
+    public function __construct($publicKey) {
+        $this->publicKey = $publicKey;
+    }
+
+    public function request($action = 'track', $data = array()) {
+        return $this->getContentData($this->apiUrl . $action, $data);
+    }
+
+    private function getContentData($url, $data = array()) {
+						
+		if(!function_exists('json_encode') || !function_exists('file_get_contents')){
+			return false;
+		}
+
+        $ch = curl_init();
+		
+		//
+		
+		$data['projectPublicKey'] = $this->publicKey;
+		
+        $url = $url . '?data=' . base64_encode(json_encode($data));
+		
+		//
+								
+        $response = file_get_contents($url);
+		
+        return $response;
+    }
+
+}
